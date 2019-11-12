ee88aec60e9bb9485668af8a76bda5c09cbb931a
Joe Dupuis <joe.dupuis@xylio.net>
2019-11-12 17:19:29 -0500

    Avahi

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,13 @@
   networking.networkmanager.enable = true;
 
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish.enable = true;
+    publish.addresses = true;
+  };
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
