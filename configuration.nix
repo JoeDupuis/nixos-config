f7abb8ca9d3ac3dd9f215458aa32e74c01271ec0
Joe Dupuis <joe.dupuis@xylio.net>
2019-11-24 19:52:12 -0500

    No password auth

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
+  services.openssh.passwordAuthentication = false;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
