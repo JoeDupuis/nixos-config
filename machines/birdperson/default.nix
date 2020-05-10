0a8461859b45b48f309077ad1f27915a39666f90
Joe Dupuis <joe@dupuis.io>
2020-05-10 18:28:11 -0400

    Support multi gpu VM with different build config at boot

--- machines/birdperson/default.nix
+++ machines/birdperson/default.nix
@@ -8,7 +8,12 @@
 
   networking.hostName = "birdperson";
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "nodev";
+    efiSupport = true;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
 
