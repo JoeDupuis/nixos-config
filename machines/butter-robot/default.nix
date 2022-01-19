c3e1c608595acd0eb19ac10193f515250d278aa8
Joe Dupuis <joe@dupuis.io>
2022-01-18 22:13:32 -0800

    Butter-robot

---
+++ machines/butter-robot/default.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../profiles/laptop.nix
+    ../../modules/bluetooth.nix
+  ];
+
+  networking.hostName = "butter-robot";
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "nodev";
+    efiSupport = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "21.05"; # Did you read the comment?
+}
