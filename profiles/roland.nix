abfd41e116be39dea9b11eaa2f657490d3cb4f7a
Joe Dupuis <joe@dupuis.io>
2022-03-28 16:24:51 -0700

    Remove virt

--- profiles/roland.nix
+++ profiles/roland.nix
@@ -124,11 +124,6 @@
   services.xrdp.enable = true;
   services.xrdp.defaultWindowManager = 	"/run/current-system/sw/bin/xfce4-session";
 
-  virtualisation = {
-    libvirtd.enable = true;
-    libvirtd.qemuOvmf = true;
-  };
-
   # Needed for nixops libvirtd backend
   # https://nixos.org/nixops/manual/#idm140737322394336
   networking.firewall.checkReversePath = false;
