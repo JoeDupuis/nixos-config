48fac994442828e264e69849e6fd4a67927686da
Joe Dupuis <joe@dupuis.io>
2024-11-29 20:42:51 -0800

    Fix tinkles network

--- machines/tinkles/default.nix
+++ machines/tinkles/default.nix
@@ -19,8 +19,8 @@
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
