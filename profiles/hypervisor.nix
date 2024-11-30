e435073ffb6e4fe8e3fe319ba6cc1734302965fd
Joe Dupuis <joe@dupuis.io>
2024-11-30 15:52:36 -0800

    Incus firewall

--- profiles/hypervisor.nix
+++ profiles/hypervisor.nix
@@ -13,5 +13,11 @@
     enable = true;
     ui.enable = true;
   };
+
+  #incus ui
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 8443 ];
+
+  networking.firewall.trustedInterfaces = [ "incusbr0" ];
+
   networking.nftables.enable = true;
 }
