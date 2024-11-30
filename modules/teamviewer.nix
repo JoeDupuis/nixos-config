b138e3be1c966d23295a8d2be872b49cd4c48f9e
Joé Dupuis <joe@dupuis.io>
2024-11-30 00:59:39 -0800

    Teamviewer off

--- modules/teamviewer.nix
+++ modules/teamviewer.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   #need the services for it work
-  services.teamviewer.enable   = true;
+  services.teamviewer.enable   = false;
   environment.systemPackages = with pkgs; [teamviewer];
   #networking.firewall.allowedTCPPorts = [ 5938 ];
   #networking.firewall.allowedUDPPorts = [ 5938 ];
