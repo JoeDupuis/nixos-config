415ac7e005fff5388f4f8970c9561628677f93b5
Joé Dupuis <joe@dupuis.io>
2024-12-07 21:55:47 -0800

    Installer generator

---
+++ support/installer/default.nix
@@ -0,0 +1,16 @@
+let
+  pkgs = import <nixpkgs>;
+  nixos = import <nixpkgs/nixos>;
+in
+(
+  nixos {
+    configuration = {
+      imports = [
+        <nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
+        <nixpkgs/nixos/modules/profiles/all-hardware.nix>
+        <nixpkgs/nixos/modules/profiles/base.nix>
+        ./configuration.nix
+      ];
+    };
+  }
+).config.system.build.isoImage
