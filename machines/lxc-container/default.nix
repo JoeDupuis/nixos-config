2dc04e4dbcf4ea01dc3fe3505c8ddb5bb2386e09
Joé Dupuis <joe@dupuis.io>
2024-12-08 11:12:31 -0800

    incus container network

--- machines/lxc-container/default.nix
+++ machines/lxc-container/default.nix
@@ -5,5 +5,24 @@
     ../../profiles/kamal.nix
   ];
 
-  system.stateVersion = lib.mkDefault lib.trivial.release;
+
+  networking = {
+    dhcpcd.enable = false;
+    useDHCP = false;
+    useHostResolvConf = false;
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."50-eth0" = {
+      matchConfig.Name = "eth0";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
+
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
