1daa11e052ae1c7fa85a39217f21fe920a04503d
Joé Dupuis <joe@dupuis.io>
2024-12-08 11:43:06 -0800

    More incus instructions

--- support/incus/default.nix
+++ support/incus/default.nix
@@ -20,3 +20,8 @@ in
 # nix-build -A container
 # incus image import --alias nixos/custom/container result/tarball/nixos-system-x86_64-linux.tar.xz result-2/nixos-lxc-image-x86_64-linux.squashfs
 # incus launch nixos/custom/container customnixos -c security.nesting=true
+#
+# copy-config
+# passwd <user>
+# Set the hostname in incus.nix
+# tailscale up
