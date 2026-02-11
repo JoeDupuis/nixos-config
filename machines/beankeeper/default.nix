9c17da96f97d2e034bd5b0ec6b30c436b372dafc
Joé Dupuis <joe@dupuis.io>
2026-02-10 20:13:03 -0800

    Pause beankeeper backups

--- machines/beankeeper/default.nix
+++ machines/beankeeper/default.nix
@@ -6,18 +6,18 @@
     ../lxc-container
   ];
 
-  services.restic.backups.beankeeper = {
-    initialize = true;
-    environmentFile = "/etc/restic/env";
-    repositoryFile = "/etc/restic/repo";
-    paths = ["/var/lib/docker/volumes"];
-    passwordFile = "/etc/restic/password";
-    pruneOpts = [
-      "--keep-last 100"
-      "--keep-daily 90"
-    ];
-    timerConfig = {
-      OnCalendar = "*:0/15";
-    };
-  };
+  # services.restic.backups.beankeeper = {
+  #   initialize = true;
+  #   environmentFile = "/etc/restic/env";
+  #   repositoryFile = "/etc/restic/repo";
+  #   paths = ["/var/lib/docker/volumes"];
+  #   passwordFile = "/etc/restic/password";
+  #   pruneOpts = [
+  #     "--keep-last 100"
+  #     "--keep-daily 90"
+  #   ];
+  #   timerConfig = {
+  #     OnCalendar = "*:0/15";
+  #   };
+  # };
 }
